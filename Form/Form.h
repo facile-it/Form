3f31b49bb465eab92a73ecd430c6d3fc50fa1064
Elviro Rocca <elviro.rocca@facile.it>
2016-09-30 15:57:44 +0200

    Initial Commit

---
+++ Form/Form.h
@@ -0,0 +1,19 @@
+//
+//  Form.h
+//  Form
+//
+//  Created by Elviro Rocca on 30/09/16.
+//  Copyright © 2016 Facile.it. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Form.
+FOUNDATION_EXPORT double FormVersionNumber;
+
+//! Project version string for Form.
+FOUNDATION_EXPORT const unsigned char FormVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Form/PublicHeader.h>
+
+
