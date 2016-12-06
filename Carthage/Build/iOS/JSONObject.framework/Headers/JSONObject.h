84056d3d4c1f698bfe8a22084f5b488856126000
Elviro Rocca <elviro.rocca@facile.it>
2016-12-06 17:39:01 +0100

    libraries updated

---
+++ Carthage/Build/iOS/JSONObject.framework/Headers/JSONObject.h
@@ -0,0 +1,19 @@
+//
+//  JSONObject.h
+//  JSONObject
+//
+//  Created by Elviro Rocca on 25/10/16.
+//  Copyright © 2016 broomburgo.it. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSONObject.
+FOUNDATION_EXPORT double JSONObjectVersionNumber;
+
+//! Project version string for JSONObject.
+FOUNDATION_EXPORT const unsigned char JSONObjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSONObject/PublicHeader.h>
+
+
