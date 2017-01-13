fd08d11d8afaa80c11b740d4bbce56ef17080103
Christian Miranti <christian.miranti@facile.it>
2017-01-13 17:35:21 +0100

    added test for monads laws of FieldCondition

--- FormSpec/FieldConditionSpec.swift
+++ FormSpec/FieldConditionSpec.swift
@@ -11,5 +11,13 @@ class FieldConditionSpec: XCTestCase {
         property("1•a = a") <- forAll { (av: OptionalOf<Int>, object: TestedCondition) in
             TestedCondition.empty.join(object).isEqual(to: object) § av.getOptional
         }
+        
+        property("a•1 = a") <- forAll { (av: OptionalOf<Int>, object: TestedCondition) in
+            object.join(TestedCondition.empty).isEqual(to: object) § av.getOptional
+        }
+        
+        property("(a•b)•c = a•(b•c)") <- forAll { (av: OptionalOf<Int>, object1: TestedCondition, object2: TestedCondition, object3: TestedCondition) in
+            (object1.join(object2)).join(object3).isEqual(to:object1.join(object2.join(object3))) § av.getOptional
+        }
     }
 }
